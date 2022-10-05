ef8c5980622676476857651d0f354990a45a974a
Peramanathan007 <105202026+Peramanathan007@users.noreply.github.com>
2022-10-06 00:35:52 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -2,8 +2,7 @@ public class Helloworld {
             public static void main(String[] args) {
             System.out.println("Hello World");
             System.out.println("Hello mars");
-            System.out.println("Hello Jupitor");
-            System.out.println("Vanakam da mapla");//newly added            
+            System.out.println("Hello Jupitor");           
             }
             }
             
