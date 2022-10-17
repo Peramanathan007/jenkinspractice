4f4fca9d07b91e3c88c4e4db730151cf5cdf3075
Peramanathan007 <105202026+Peramanathan007@users.noreply.github.com>
2022-10-17 16:27:37 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -3,6 +3,7 @@ public class simple {
             System.out.println("Hello World");
             System.out.println("Hello mars");
             System.out.println("Hello Jupitor"); 
+            System.out.println("Sakthi");
 
 
            }
